9c472e12679ed09f485b6d9253b4c8a3bfbfd9cf
emirsavran <emir@emirsavran.com>
2017-12-15 00:52:47 +0300

    Added nickname support for chat

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,20 @@
 //Create a new Client object with your broker's hostname, port and your own clientId
 var client = new Messaging.Client("broker.mqttdashboard.com", 8000, "myclientid_" + parseInt(Math.random() * 100, 10));
 
+var nick;
+
+function promptNick () {
+  var nickprompt;
+  while (nickprompt == "" || nickprompt == null) {
+    nickprompt = prompt("Enter your nickname:");
+  }
+  return nickprompt;
+}
+
+setTimeout(() => {
+  nick = promptNick();
+},100);
+
 var messagesDiv = document.getElementById('messages');
 
 var messageArea = document.getElementById('message');
@@ -10,7 +24,7 @@ var clearBtn = document.getElementById('clear');
 var options = {
 
   //connection attempt timeout in seconds
-  timeout: 0,
+  timeout: 2,
 
   //Gets Called if the connection has successfully been established
   onSuccess: function () {
@@ -29,14 +43,13 @@ client.connect(options);
 setTimeout(function() {
   client.subscribe('emir', { qos: 2 });
   messagesDiv.innerHTML = `<p class="bg-primary message">Subscribed.</p>` + messagesDiv.innerHTML;
-},2000);
+},3000);
 
 //Gets called whenever you receive a message for your subscriptions
 client.onMessageArrived = function (message) {
   //Do something with the push message you received
-  // $('#messages').append('<span>Topic: ' + message.destinationName + '  | ' + message.payloadString + '</span><br/>');
-
-  messagesDiv.innerHTML = `<p class="bg-primary message">${message.payloadString}</p>` + messagesDiv.innerHTML;
+  var msg = JSON.parse(message.payloadString);
+  messagesDiv.innerHTML = `<div class="bg-primary message"><h4>Sender: ${msg.nickname}</h4><p>${msg.message}</p></div>` + messagesDiv.innerHTML;
 };
 
 var publish = function (payload, topic, qos) {
@@ -49,7 +62,7 @@ var publish = function (payload, topic, qos) {
 
 message.addEventListener("keydown", function (event) {
   if (event.which == 13 || event.keyCode == 13) {
-    publish(message.value, 'emir', 2);
+    publish(`{"message": "${message.value}", "nickname": "${nick}"}`, 'emir', 2);
     message.value = "";
   }
 });
